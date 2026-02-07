b2f2cd17c735278c87fccaf149dff0894a7b48e4
Bartjan Henkemans <bartjan@henkemans.be>
2026-02-07 16:27:14 +0100

    Firefox

---
+++ users/team/browser.nix
@@ -0,0 +1,30 @@
+{ pkgs, ... }: 
+let
+  dj_url = "https://judge.gehack.nl";
+  devdocs_url = "https://devdocs.io/";
+in
+{
+  programs.firefox = {
+    enable = true;
+    policies = {
+      Homepage = {
+        URL = dj_url;
+        Locked = false;
+        StartPage = "homepage";
+      };
+      DisplayBookmarksToolbar = "always";
+      Bookmarks = [
+        {
+          Title = "DOMjudge";
+          URL = dj_url;
+          Placement = "toolbar";
+        }
+        {
+          Title = "DevDocs";
+          URL = devdocs_url;
+          Placement = "toolbar";
+        }
+      ];
+    };
+  };
+}
