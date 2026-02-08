bac6a2d2df8ede265c82a2e24ad204de98883891
Bartjan Henkemans <bartjan@henkemans.be>
2026-02-08 16:20:04 +0100

    Disable sleep, suspend, hibernate and hybrid-sleep

--- modules/teammachine/gnome.nix
+++ modules/teammachine/gnome.nix
@@ -21,4 +21,9 @@
     gnome-terminal
     nautilus
     ];
+  
+  systemd.targets.sleep.enable = false;
+  systemd.targets.suspend.enable = false;
+  systemd.targets.hibernate.enable = false;
+  systemd.targets.hybrid-sleep.enable = false;
 }
