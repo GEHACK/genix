283f36a4b14783c85e097f8246de526d91ff1987
Bartjan Henkemans <bartjan@henkemans.be>
2026-02-09 18:40:18 +0100

    Reverted to bash for contest user

--- modules/teammachine/users.nix
+++ modules/teammachine/users.nix
@@ -5,8 +5,5 @@
     # TODO: encrypt a strong password with sops nix
     initialPassword = "gehackgehack";
     openssh.authorizedKeys.keyFiles = [ ../../authorized_keys ];
-    shell = pkgs.zsh;
   };
-
-  programs.zsh.enable = true;
 }
