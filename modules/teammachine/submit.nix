297815cc83fc6f46e962f764178e64a6bd2372e3
Bartjan Henkemans <bartjan@henkemans.be>
2026-02-10 16:47:26 +0100

    submit client

---
+++ modules/teammachine/submit.nix
@@ -0,0 +1,37 @@
+{ pkgs, lib, ... }:
+
+let
+  domjudge-submit = pkgs.python3Packages.buildPythonApplication {
+    pname = "domjudge-submit";
+    version = "git";
+    format = "other";
+
+    src = pkgs.fetchFromGitHub {
+      owner = "domjudge";
+      repo = "domjudge";
+      rev = "main"; 
+      sha256 = "sha256-rLYfSmDrQBbaDkTdex/tD+OuaY5Ehqj+7+paiFHTT7o="; 
+    };
+
+    propagatedBuildInputs = with pkgs.python3Packages; [
+      requests
+      python-magic
+    ];
+
+    dontBuild = true;
+    dontConfigure = true;
+
+    installPhase = ''
+      install -Dm755 submit/submit $out/bin/submit
+    '';
+
+    meta = with lib; {
+      mainProgram = "submit";
+    };
+  };
+in
+{
+  environment.systemPackages = [
+    domjudge-submit
+  ];
+}
