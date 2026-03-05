e2057ba20e3eb568d9cb12e62084fc5deb9c04b0
Bartjan Henkemans <bartjan@henkemans.be>
2026-03-05 18:18:53 +0100

    feat: Firewall

--- modules/teammachine/networking.nix
+++ modules/teammachine/networking.nix
@@ -5,7 +5,18 @@
     extraHosts = 
       ''
         127.0.0.1 docs
+        10.0.0.1 judge
       '';
+
+    # Disable the default iptables-based firewall manager
+    firewall.enable = false;
+
+    # Enable native nftables and provide the raw ruleset
+    nftables = {
+      enable = true;
+      checkRuleset = true;
+      rulesetFile = ./assets/firewall.nft;
+    };
   };
   
   boot.blacklistedKernelModules = [ "iwlwifi" "btusb" ];
