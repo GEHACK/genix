bdbd5381f5e40ee88f3e5a4bfebfcd5146de65de
Bartjan Henkemans <bartjan@henkemans.be>
2026-03-05 19:59:39 +0100

    Removed nvidia settings from apps

--- users/team/gnome.nix
+++ users/team/gnome.nix
@@ -1,4 +1,14 @@
 _: {
+
+  xdg.desktopEntries = {
+    # The key here must match the exact filename of the system's .desktop file (without the .desktop extension)
+    "nvidia-settings" = {
+      name = "NVIDIA X Server Settings";
+      exec = "nvidia-settings";
+      noDisplay = true; # This is the magic line that hides it from the app grid
+    };
+  };
+
   dconf = {
     enable = true;
     settings = {
