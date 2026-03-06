2d6472dd3b382b21826c3f761152870656c6ea4f
Luuk Blankenstijn <git@luukblankenstijn.nl>
2026-03-06 22:20:57 +0100

    feature: loom authentication

--- modules/teammachine/loom.nix
+++ modules/teammachine/loom.nix
@@ -1,6 +1,9 @@
-_: {
+{ config, ... }:
+{
+  sops.secrets.loom-auth = { };
   services.loomd = {
     enable = true;
-    server = "http://loom.gehack.nl";
+    server = "http://192.168.122.1:8080";
+    authTokenCommand = "cat ${config.sops.secrets.loom-auth.path}";
   };
 }
