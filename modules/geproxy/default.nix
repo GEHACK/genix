86b23ab9f84bedfd6d8748e3acc74168e33953d8
Luuk Blankenstijn <git@luukblankenstijn.nl>
2026-04-18 11:25:48 +0200

    feature: add utility packages to geproxy

--- modules/geproxy/default.nix
+++ modules/geproxy/default.nix
@@ -1,8 +1,21 @@
-_: {
+{ pkgs, ... }:
+{
   imports = [
     ./boot.nix
     ./fog.nix
     ./networking.nix
     ./traefik.nix
+  ];
+
+  environment.systemPackages = with pkgs; [
+    wget
+
+    zip
+    unzip
+
+    btop
+    htop
+
+    git
   ];
 }
