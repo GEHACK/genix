8dd93bd30f5d3f88d1634394cb251a2e130ae40a
Luuk Blankenstijn <git@luukblankenstijn.nl>
2026-05-02 10:37:08 +0200

    cuproxy

--- modules/geproxy/cuproxy.nix
+++ modules/geproxy/cuproxy.nix
@@ -1,11 +1,13 @@
-_: {
+{ pkgs, ... }:
+{
   services.cuproxy = {
     enable = true;
-    logLevel = "trace";
+    logLevel = "info";
     printerTo = "10.0.0.10:631/ipp/print";
     settings = {
-      WEBHOOKS_TO_CALL = "info;GET;https://loom.gehack.nl/team-info/10.0.0.92|logo;GET;https://euc-static.gehack.nl/logos/black.png";
-      TYPST_TEMPLATE = ./assets/print_template.typ;
+      WEBHOOKS_TO_CALL = "info;GET;https://loom.gehack.nl/team-info/{{requesting_ip}}&&map;GET;https://loom.gehack.nl/map-image?ip={{requesting_ip}}";
+      TYPST_TEMPLATE = "${./assets/print_template.typ}";
+      TYPST_BIN = "${pkgs.typst}/bin/typst";
     };
   };
 }
