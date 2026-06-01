2164c85050d25e458a7770c8bd85d0e6fd205fcc
Luuk Blankenstijn <git@luukblankenstijn.nl>
2026-06-01 21:48:11 +0200

    fix: add /api for new loom endpoints

--- modules/geproxy/cuproxy.nix
+++ modules/geproxy/cuproxy.nix
@@ -5,7 +5,7 @@
     logLevel = "info";
     printerTo = "10.0.0.10:631/ipp/print";
     settings = {
-      WEBHOOKS_TO_CALL = "info;GET;https://loom.gehack.nl/team-info/{{requesting_ip}}&&map;GET;https://loom.gehack.nl/map-image?ip={{requesting_ip}}";
+      WEBHOOKS_TO_CALL = "info;GET;https://loom.gehack.nl/api/team-info/{{requesting_ip}}&&map;GET;https://loom.gehack.nl/api/map-image?ip={{requesting_ip}}";
       TYPST_TEMPLATE = "${./assets/print_template.typ}";
       TYPST_BIN = "${pkgs.typst}/bin/typst";
     };
