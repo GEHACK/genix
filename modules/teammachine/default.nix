9319eba0875ed98a62453194f4696c30f060a4f4
Bartjan Henkemans <bartjan@henkemans.be>
2026-06-01 21:21:19 +0200

    feat: Games

--- modules/teammachine/default.nix
+++ modules/teammachine/default.nix
@@ -2,6 +2,7 @@ _: {
   imports = [
     ./boot.nix
     ./desktop.nix
+    ./games.nix
     ./greeter.nix
     ./ides.nix
     ./languages.nix
---
+++ modules/teammachine/games.nix
@@ -0,0 +1,69 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  cfg = config.gehack.teammachine.games;
+in
+{
+  options.gehack.teammachine.games.enable =
+    lib.mkEnableOption "downtime games on the teammachine"
+    // {
+      default = false;
+    };
+
+  config = lib.mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [
+      # Terminal classics
+      ninvaders
+      nsnake
+      moon-buggy
+      bsdgames
+
+      # GNOME games (gnome.games.enable is off, so add individually)
+      aisleriot
+      gnome-mines
+      gnome-mahjongg
+      gnome-sudoku
+      quadrapassel
+      gnome-2048
+      five-or-more
+      four-in-a-row
+
+      # Puzzle / strategy
+      sgt-puzzles
+
+      # Retro arcade
+      supertux
+      supertuxkart
+      neverball
+    ];
+
+    # Group every game .desktop entry into a single "Games" folder in the
+    # GNOME app grid so the contest tools stay uncluttered.
+    home-manager.users.team.dconf.settings = {
+      "org/gnome/desktop/app-folders" = {
+        folder-children = [ "Games" ];
+      };
+      "org/gnome/desktop/app-folders/folders/Games" = {
+        name = "Games";
+        apps = [
+          "sol.desktop"
+          "org.gnome.Mines.desktop"
+          "org.gnome.Mahjongg.desktop"
+          "org.gnome.Sudoku.desktop"
+          "org.gnome.Quadrapassel.desktop"
+          "org.gnome.TwentyFortyEight.desktop"
+          "org.gnome.five-or-more.desktop"
+          "org.gnome.Four-in-a-row.desktop"
+          "sgt-puzzles.desktop"
+          "supertux2.desktop"
+          "supertuxkart.desktop"
+          "neverball.desktop"
+        ];
+      };
+    };
+  };
+}
