c2def9725f838bb2c10cb7bcd3baf9385eb50ebe
Luuk Blankenstijn <git@luukblankenstijn.nl>
2026-06-01 21:46:39 +0200

    feature: add printer ppd file so the ensure printers service does not crash when the printer is not available

--- modules/teammachine/printer.nix
+++ modules/teammachine/printer.nix
@@ -1,25 +1,28 @@
-{ pkgs, ... }: {
+{ pkgs, lib, ... }:
+{
   services.printing = {
     enable = true;
-    drivers = with pkgs; [
-      cups-filters
-      cups-browsed
-    ];
+    drivers = lib.singleton (
+      pkgs.linkFarm "drivers" [
+        {
+          name = "share/cups/model/PSGEWIS1.ppd";
+          path = ./assets/PSGEWIS1.ppd;
+        }
+
+      ]
+    );
   };
 
-hardware.printers = {
- ensurePrinters = [
-   {
-     name = "PSGEWIS1";
-     deviceUri = "ipp://10.0.0.1:631/ipp/print";
-     model = "everywhere";
-     # ppdOptions = {
-     #   PageSize = "A4";
-     # };
-   }
- ];
- ensureDefaultPrinter = "PSGEWIS1";
-};
+  hardware.printers = {
+    ensurePrinters = [
+      {
+        name = "PSGEWIS1";
+        deviceUri = "ipp://10.0.0.1:631/ipp/print";
+        model = "PSGEWIS1.ppd";
+      }
+    ];
+    ensureDefaultPrinter = "PSGEWIS1";
+  };
 
   systemd.services.ensure-printers = {
     serviceConfig = {
@@ -31,5 +34,4 @@ hardware.printers = {
       StartLimitIntervalSec = 300;
     };
   };
-
 }
