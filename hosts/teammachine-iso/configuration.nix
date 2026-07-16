0cf73d0f129763768abe04bf0a3e5abad2cec80f
Bartjan Henkemans <bartjan@henkemans.be>
2026-07-16 20:09:33 +0200

    Removed secrets, added autologin

--- hosts/teammachine-iso/configuration.nix
+++ hosts/teammachine-iso/configuration.nix
@@ -7,25 +7,17 @@
 {
   imports = [
     (modulesPath + "/installer/cd-dvd/iso-image.nix")
-
     ../../modules
-
-    # Full teammachine module set minus boot.nix (ISO provides its own bootloader)
     ../../modules/teammachine/desktop.nix
-    ../../modules/teammachine/greeter.nix
     ../../modules/teammachine/locale.nix
-    ../../modules/teammachine/loom.nix
     ../../modules/teammachine/networking.nix
     ../../modules/teammachine/printer.nix
     ../../modules/teammachine/pxe-boot.nix
     ../../modules/teammachine/usbguard.nix
     ../../modules/teammachine/user-tools.nix
-    ../../modules/teammachine/users.nix
     ../../modules/teammachine/webcamstream.nix
   ];
 
-  # Mirror the teammachine host: per-user contest tooling is toggled via
-  # teammachine.users.<name> and forwarded to home-manager by user-tools.nix.
   teammachine = {
     users.team = {
       languages = {
@@ -48,15 +40,43 @@
     };
   };
 
-  # Decrypt secrets-iso.yaml using the iso-key baked into the image
-  sops = {
-    defaultSopsFile = lib.mkForce ../../secrets-iso.yaml;
-    age.keyFile = lib.mkForce "/etc/sops/hostkey";
+  disabledModules = [ ../../modules/users.nix ];
+
+  users.mutableUsers = false;
+  security.sudo.wheelNeedsPassword = false;
+  security.pam.services.su.requireWheel = true;
+  programs.zsh.enable = true;
+
+  users.users.root.openssh.authorizedKeys.keyFiles = [
+    ../../authorized_keys
+    ../../fanout_pubkey
+  ];
+
+  users.users.team = {
+    isNormalUser = true;
+    hashedPassword = "$y$j9T$GKsBsZ1X7y96QWFjU8j1x0$VaXm4KxSISIUZk49R7QQXc6opKxDKHOiXPA27zl6Zw8";
+    openssh.authorizedKeys.keyFiles = [ ../../authorized_keys ];
   };
-  environment.etc."sops/hostkey" = {
-    source = ../../iso-key;
-    mode = "0400";
+
+  users.users.gehack = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ];
+    hashedPassword = "$y$j9T$GKsBsZ1X7y96QWFjU8j1x0$VaXm4KxSISIUZk49R7QQXc6opKxDKHOiXPA27zl6Zw8";
+    openssh.authorizedKeys.keyFiles = [ ../../authorized_keys ];
+    shell = pkgs.zsh;
   };
+
+  # No greeter: GDM autologins the team user straight into GNOME.
+  services.displayManager = {
+    gdm.enable = lib.mkForce true;
+    autoLogin = {
+      enable = true;
+      user = "team";
+    };
+  };
+  # GNOME autologin races with getty on tty1 unless these are disabled.
+  systemd.services."getty@tty1".enable = false;
+  systemd.services."autovt@tty1".enable = false;
 
   image.fileName = lib.mkForce "gehack-teammachine.iso";
   isoImage = {
