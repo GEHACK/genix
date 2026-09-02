96d87de54f5f4c214fddf484ceb8763c76f728f0
Luuk Blankenstijn <git@luukblankenstijn.nl>
2026-09-02 20:21:20 +0200

    feat: install zsh omz path:lib plugin for better shell navigation

--- users/gehack/shell.nix
+++ users/gehack/shell.nix
@@ -6,8 +6,15 @@
       autosuggestion.enable = true;
       enableCompletion = true;
       syntaxHighlighting.enable = true;
-      # basic fast prompt styling
+      antidote = {
+        enable = true;
+        plugins = [
+          "getantidote/use-omz"
+          "ohmyzsh/ohmyzsh path:lib"
+        ];
+      };
       initContent = ''
+        DISABLE_AUTO_UPDATE="true"
         fpath+=("${pkgs.pure-prompt}/share/zsh/site-functions")
         autoload -Uz promptinit; promptinit
         prompt pure
