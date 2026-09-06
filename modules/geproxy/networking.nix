48725cea5bba103f56dd72d257db0142f0f0e3e0
Luuk Blankenstijn <git@luukblankenstijn.nl>
2026-09-06 13:08:06 +0200

    feat: proxy *.team.loom dns requests to loom

--- modules/geproxy/networking.nix
+++ modules/geproxy/networking.nix
@@ -64,90 +64,106 @@
   users.groups.dnsmasq = {
     gid = 995;
   };
+  services = {
+    # dnsmasq configuration
+    dnsmasq = {
+      resolveLocalQueries = false;
+      enable = true;
+      settings = {
+        # Logging
+        log-queries = true;
+        log-dhcp = true;
 
-  # dnsmasq configuration
-  services.dnsmasq = {
-    resolveLocalQueries = false;
-    enable = true;
-    settings = {
-      # Logging
-      log-queries = true;
-      log-dhcp = true;
+        domain-needed = true;
+        bogus-priv = true;
 
-      domain-needed = true;
-      bogus-priv = true;
+        bind-interfaces = true;
+        interface = [
+          "br-contest"
+          "br-admin"
+        ];
+        except-interface = "wlp6s0";
 
-      bind-interfaces = true;
-      interface = [
-        "br-contest"
-        "br-admin"
-      ];
-      except-interface = "wlp6s0";
+        # TFTP chainloads iPXE; iPXE then fetches the imaged boot script over HTTP.
+        enable-tftp = true;
+        tftp-root = "${pkgs.ipxe}";
 
-      # TFTP chainloads iPXE; iPXE then fetches the imaged boot script over HTTP.
-      enable-tftp = true;
-      tftp-root = "${pkgs.ipxe}";
+        # DHCP
+        dhcp-authoritative = true;
 
-      # DHCP
-      dhcp-authoritative = true;
+        # Listen addresses
+        listen-address = [
+          "10.0.0.1"
+          "10.0.1.1"
+        ];
 
-      # Listen addresses
-      listen-address = [
-        "10.0.0.1"
-        "10.0.1.1"
-      ];
+        # Domains
+        domain = [
+          "contest.local,br-contest"
+        ];
 
-      # Domains
-      domain = [
-        "contest.local,br-contest"
-      ];
+        # DHCP ranges
+        dhcp-range = [
+          "br-contest,10.0.0.50,10.0.0.250,255.255.255.0,infinite"
+          "br-admin,10.0.1.50,10.0.1.250,255.255.255.0,infinite"
+        ];
 
-      # DHCP ranges
-      dhcp-range = [
-        "br-contest,10.0.0.50,10.0.0.250,255.255.255.0,infinite"
-        "br-admin,10.0.1.50,10.0.1.250,255.255.255.0,infinite"
-      ];
+        # DHCP options
+        dhcp-option = [
+          # Contest: captive, everything through 10.0.0.1
+          "br-contest,3,10.0.0.1"
+          "br-contest,6,10.0.0.1"
+          "br-contest,42,10.0.0.1"
+          # Admin: real gateway + public DNS so it bypasses dnsmasq
+          "br-admin,3,10.0.1.1"
+          "br-admin,6,8.8.8.8,1.1.1.1"
+          "br-admin,42,10.0.1.1"
+        ];
 
-      # DHCP options
-      dhcp-option = [
-        # Contest: captive, everything through 10.0.0.1
-        "br-contest,3,10.0.0.1"
-        "br-contest,6,10.0.0.1"
-        "br-contest,42,10.0.0.1"
-        # Admin: real gateway + public DNS so it bypasses dnsmasq
-        "br-admin,3,10.0.1.1"
-        "br-admin,6,8.8.8.8,1.1.1.1"
-        "br-admin,42,10.0.1.1"
-      ];
+        # DNS addresses
+        address = [
+          "/judge.gehack.nl/10.0.0.1"
+          "/imaged.gehack.nl/10.0.0.1"
+          "/loom.gehack.nl/10.0.0.1"
+          "/cds.gehack.nl/10.0.0.1"
+          "/docs.gehack.nl/10.0.0.1"
+        ];
 
-      # DNS addresses
-      address = [
-        "/judge.gehack.nl/10.0.0.1"
-        "/imaged.gehack.nl/10.0.0.1"
-        "/loom.gehack.nl/10.0.0.1"
-        "/cds.gehack.nl/10.0.0.1"
-        "/docs.gehack.nl/10.0.0.1"
-      ];
+        # PXE/imaged boot configuration
+        dhcp-userclass = "set:ipxe,iPXE";
+        dhcp-match = [
+          "set:bios,60,PXEClient:Arch:00000"
+          "set:efi32,60,PXEClient:Arch:00006"
+          "set:efibc,60,PXEClient:Arch:00007"
+          "set:efi64,60,PXEClient:Arch:00009"
+        ];
+        dhcp-boot = [
+          "tag:!ipxe,tag:bios,undionly.kpxe,,10.0.0.1"
+          "tag:!ipxe,tag:efi32,ipxe.efi,,10.0.0.1"
+          "tag:!ipxe,tag:efibc,ipxe.efi,,10.0.0.1"
+          "tag:!ipxe,tag:efi64,ipxe.efi,,10.0.0.1"
+          "tag:ipxe,http://10.0.0.1:8080/boot/boot.ipxe"
+        ];
 
-      # PXE/imaged boot configuration
-      dhcp-userclass = "set:ipxe,iPXE";
-      dhcp-match = [
-        "set:bios,60,PXEClient:Arch:00000"
-        "set:efi32,60,PXEClient:Arch:00006"
-        "set:efibc,60,PXEClient:Arch:00007"
-        "set:efi64,60,PXEClient:Arch:00009"
-      ];
-      dhcp-boot = [
-        "tag:!ipxe,tag:bios,undionly.kpxe,,10.0.0.1"
-        "tag:!ipxe,tag:efi32,ipxe.efi,,10.0.0.1"
-        "tag:!ipxe,tag:efibc,ipxe.efi,,10.0.0.1"
-        "tag:!ipxe,tag:efi64,ipxe.efi,,10.0.0.1"
-        "tag:ipxe,http://10.0.0.1:8080/boot/boot.ipxe"
-      ];
+        dhcp-host = [
+          "b0:0c:d1:de:f0:0d,10.0.0.10,infinite"
+          "02:20:d7:6f:20:cc,10.0.0.11,infinite"
+        ];
+      };
+    };
 
-      dhcp-host = [
-        "b0:0c:d1:de:f0:0d,10.0.0.10,infinite"
-        "02:20:d7:6f:20:cc,10.0.0.11,infinite"
+    # proxy team.loom to loom to resolve the ip of some team
+    dnsmasq.settings.server = [ "/team.loom/127.0.0.1#5053" ];
+
+    https-dns-proxy = {
+      enable = true;
+      address = "127.0.0.1";
+      port = 5053;
+      extraArgs = [
+        "-r"
+        "https://loom.gehack.nl/api/dns-query"
+        "-b"
+        "127.0.0.1"
       ];
     };
   };
