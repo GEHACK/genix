d88b96cefcd021ce4547ff1ebe07eb272cb24188
Luuk Blankenstijn <git@luukblankenstijn.nl>
2026-09-18 22:45:51 +0200

    update contest id

--- flake.nix
+++ flake.nix
@@ -59,7 +59,7 @@
       admin_ip = "10.0.1.1";
       admin_subnet = "10.0.1.0/24";
       imaged_port = 8080;
-      contest_id = "prelims";
+      contest_id = "prelims-test-session";
       specialArgs = {
         inherit
           dj_url
--- modules/cds/mdns.nix
+++ modules/cds/mdns.nix
@@ -1,6 +1,7 @@
 _: {
   services.avahi = {
     enable = true;
+    ipv6 = false;
     publish = {
       enable = true;
       addresses = true;
